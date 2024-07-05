763fa3f4248c0af2ecd95708946cf50f50c97ab1
sarthik <sarthik32@gmail.com>
2024-07-05 15:02:49 +0530

    [Sarthik]modified: implemented nodemail for sedning real time Email for password reset

--- src/controllers/notes.controller.js
+++ src/controllers/notes.controller.js
@@ -4,52 +4,55 @@ import * as notesService from '../services/notes.service';
 import sequelize, { DataTypes } from '../config/database';
 const Notes = require('../models/notes')(sequelize, DataTypes);
 
-
 export const createNotes = async (req, res) => {
-    // console.log("-->", req);
-    const data = await notesService.createNote(req.body);
+  // console.log("-->", req);
+  const data = await notesService.createNote(req.body);
 
-    res.status(data.code).json(data);
-
-}
+  res.status(data.code).json(data);
+};
 export const getAllNotes = async (req, res) => {
-    const data = await notesService.getAllNote(req.body.userId);
+  const data = await notesService.getAllNote(req.body.userId);
 
-    res.status(data.code).json(data);
-
-}
+  res.status(data.code).json(data);
+};
 export const getById = async (req, res) => {
-    const data = await notesService.getById(req.params.id, req.body.userId);
+  const data = await notesService.getById(req.params.id, req.body.userId);
 
-    res.status(data.code).json(data);
-
-}
+  res.status(data.code).json(data);
+};
 export const deleteNote = async (req, res) => {
-    const data = await notesService.deleteNote(req.params.id);
+  const data = await notesService.deleteNote(req.params.id);
 
-    res.status(data.code).json(data);
-
-}
+  res.status(data.code).json(data);
+};
 export const updateNote = async (req, res) => {
-    const data = await notesService.updateNote(req.body, req.params.id, req.body.userId);
+  const data = await notesService.updateNote(
+    req.body,
+    req.params.id,
+    req.body.userId
+  );
 
-    res.status(data.code).json(data);
-}
+  res.status(data.code).json(data);
+};
 export const isArchive = async (req, res) => {
-    const data = await notesService.isArchive(req.params.id);
-    // console.log(">debug", data);
-    res.status(data.code).json(data);
-}
+  const data = await notesService.isArchive(req.params.id);
+  // console.log(">debug", data);
+  res.status(data.code).json(data);
+};
 export const isTrash = async (req, res) => {
-    const data = await notesService.isTrash(req.params.id);
-    res.status(data.code).json(data);
-}
+  const data = await notesService.isTrash(req.params.id);
+  res.status(data.code).json(data);
+};
 export const setColor = async (req, res) => {
-    const data = await notesService.setColor(req.body, req.params.id, req.body.userId);
-    res.status(data.code).json(data);
-}
+  const data = await notesService.setColor(
+    req.body,
+    req.params.id,
+    req.body.userId
+  );
+  res.status(data.code).json(data);
+};
 export const addCollab = async (req, res) => {
-    const data = await notesService.addCollab(req.body, req.params.id);
-    // console.log("-->", data);
-    res.status(data.code).json(data);
-}
+  const data = await notesService.addCollab(req.body, req.params.id);
+  // console.log("-->", data);
+  res.status(data.code).json(data);
+};
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -8,15 +8,39 @@ export const signUp = async (req, res) => {
     code: data.code,
     data: data.data,
     message: data.message
-  })
-}
+  });
+};
 
 export const signIn = async (req, res) => {
   const data = await UserService.signIn(req.body);
-  console.log("controller", data);
   res.status(data.code).json(data);
+};
+
+export const forgetPassword = async (req, res) => {
+  const { email } = req.body;
+
+  console.log("user-email", email);
+  if (!email) {
+    return res.status(HttpStatus.BAD_REQUEST).json({
+      message: 'Email is required'
+    });
+  }
+
+  try {
+    const data = await UserService.forgetPassword(email);
+    res.status(data.code).json(data);
+  } catch (error) {
+    console.error('Error in forgetPass controller:', error);
+    res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+      message: 'Internal server error'
+    });
+  }
 }
 
-export const signOut = async (req, res) => {
-
+export const resetPassword = async (req, res) => {
+  console.log("-->", res.locals.token);
+  const { password } = req.body;
+  const userId = req.body.userId;
+  const data = await UserService.resetPassword(password, userId);
+  res.status(data.code).json(data);
 }
